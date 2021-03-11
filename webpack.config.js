4af45ff2f00895e7e1aff67651460c249b4ff636
Renato Bessa <bessache@outlook.com>
2021-03-11 19:45:38 -0300

    aula Webpack Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
     resolve: {
         extensions: [ '.js', '.jsx'],
     },
+
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
